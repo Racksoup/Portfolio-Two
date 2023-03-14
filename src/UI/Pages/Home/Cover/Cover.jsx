e2dcb3ad4c1d1eb4c260445eee68eede6656a6ae
Racksoup <rack.connor1@gmail.com>
2023-03-14 13:53:38 -0400

    replace3 tiktok image

--- src/UI/Pages/Home/Cover/Cover.jsx
+++ src/UI/Pages/Home/Cover/Cover.jsx
@@ -5,7 +5,7 @@ import Twitter from '../../../../images/twitter.png';
 import Twitch from '../../../../images/twitch.png';
 import Reddit from '../../../../images/reddit.png';
 import Instagram from '../../../../images/Instagram.png';
-import Tiktok from '../../../../images/Tiktok.png';
+import Tiktok from '../../../../images/Gigi.png';
 import Discord from '../../../../images/discord.png';
 import Desk from '../../../../images/Desk.png';
 import DotArrow from '../../../../images/DotArrow.png';
